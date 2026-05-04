609887b063f726c4fbdfab90ca7a0f27c966afe5
Agnieszka Zachariasiewicz <agnieszka.kanarcia@gmail.com>
2026-05-04 02:34:31 +0200

    cart icon + badge styling

--- src/components/CartIcon/CartIcon.tsx
+++ src/components/CartIcon/CartIcon.tsx
@@ -1,3 +1,11 @@
+import { ShoppingBasket, ShoppingCart  } from 'lucide-react';
+import styles from './CartIcon.module.css'
+
 export default function CartIcon({}){
-    return <p>cart icon</p>
+    return(
+    <div className={styles.shoppingCartIcon}>
+        <ShoppingCart  />
+        <span className={styles.counterBubble}>1</span> {/*implement adding logic later later*/}
+    </div>
+    )
 }
